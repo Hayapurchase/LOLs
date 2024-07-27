6650bc2e7169c5a293c69f3bb7d533586469f373
Hayapurchase <eric.lesilau@strathmore.edu>
2024-07-27 13:05:54 +0300

    #3D intergrated

--- LOLs/main.cpp
+++ LOLs/main.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
 #include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 
 #include "shader.h"
@@ -9,17 +11,26 @@
 #include "VBO.h"
 #include "EBO.h"
 
+int width = 800;
+int height = 800;
+
+
 float vertices[] = {
     // positions          // colors           // texture coords
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+    -0.5f,  0.0f,  0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,   // top right
+    -0.5f,  0.0f, -0.5f,   0.0f, 1.0f, 0.0f,   5.0f, 0.0f,   // bottom right
+     0.5f,  0.0f, -0.5f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+     0.5f,  0.0f,  0.5f,   1.0f, 1.0f, 0.0f,   5.0f, 0.0f,    // top left 
+     0.0f,  0.8f,  0.0f,   0.0f, 0.0f, 1.0f,   2.5f, 5.0f      //tip
 };
 
 GLuint indices[] = {  // note that we start from 0!
-    0, 2, 1,   // first triangle
-    0, 3, 2    // second triangle
+    0, 1, 2,   // first triangle
+    0, 3, 2,    // second triangle
+    1, 2, 4,
+    0, 1, 4,
+    0, 3, 4,
+    3, 2, 4
 };
 
 
@@ -27,7 +38,7 @@ GLuint indices[] = {  // note that we start from 0!
 int main(void)
 {
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
     
     if (!window)
     {
@@ -44,6 +55,8 @@ int main(void)
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    glViewport(0, 0, width, height);
 
     Shader shaderprogram("default.vert", "default.frag");
 
@@ -102,6 +115,10 @@ int main(void)
     shaderprogram.Activate();
     glUniform1i(tex0Uni, 0);
 
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+
+    glEnable(GL_DEPTH_TEST);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -109,18 +126,45 @@ int main(void)
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
         /* Render here */
-        glad_glClear(GL_COLOR_BUFFER_BIT);
+        glad_glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shaderprogram.Activate();
 
-        
+        double crntTime = glfwGetTime();
+
+        if (crntTime - prevTime >= 1 / 60)
+        {
+            rotation += 0.5f;
+            prevTime = crntTime;
+        }
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view  = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        view = glm::translate(view, glm::vec3(0.05f, -0.5f, -2.0f));
+        //                    //FOV           //screen aspect ratio  //clip distance 
+        proj = glm::perspective(glm::radians(45.0f),(float)(width/height), 0.1f, 100.0f);
+
+        //sending uniform data to shader\
+
+        int modelLoc = glGetUniformLocation(shaderprogram.ID, "model");
+        glUniformMatrix4fv(modelLoc,1,GL_FALSE,glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(shaderprogram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projLoc = glGetUniformLocation(shaderprogram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
         glUniform1f(uniID, 0.5f);
         glBindTexture(GL_TEXTURE_2D,texture);
 
         VAO1.Bind();
 
-        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        glDrawElements(GL_TRIANGLES,sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
